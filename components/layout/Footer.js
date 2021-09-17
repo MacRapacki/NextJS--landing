d1aec14352008f3302677dfaba75ec5d960e2418
Maciej Rapacki <mac.rapacki@gmail.com>
2021-09-17 14:43:53 +0100

    fix footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import MediaQuery from "react-responsive";
+// import Image from "next/image";
+// import MediaQuery from "react-responsive";
 
-import Wave from "../../images/Wave.svg";
-import WaveDesktop from "../../images/Wave-desktop.svg";
+// import Wave from "../../images/Wave.svg";
+// import WaveDesktop from "../../images/Wave-desktop.svg";
 
 import style from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      <MediaQuery minWidth={500}>
+      {/* <MediaQuery minWidth={500}>
         {(matches) =>
           matches ? (
             <Image src={WaveDesktop} alt="wave" layout="responsive" />
@@ -17,7 +17,7 @@ export default function Footer() {
             <Image src={Wave} alt="wave" layout="responsive" />
           )
         }
-      </MediaQuery>
+      </MediaQuery> */}
     </footer>
   );
 }
