8a7921fa74dfffe768a45534dbc3dc5ef0ede632
Maciej Rapacki <mac.rapacki@gmail.com>
2021-09-17 14:44:57 +0100

    remove unussed code

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,23 +1,5 @@
-// import Image from "next/image";
-// import MediaQuery from "react-responsive";
-
-// import Wave from "../../images/Wave.svg";
-// import WaveDesktop from "../../images/Wave-desktop.svg";
-
 import style from "./Footer.module.css";
 
 export default function Footer() {
-  return (
-    <footer className={style.footer}>
-      {/* <MediaQuery minWidth={500}>
-        {(matches) =>
-          matches ? (
-            <Image src={WaveDesktop} alt="wave" layout="responsive" />
-          ) : (
-            <Image src={Wave} alt="wave" layout="responsive" />
-          )
-        }
-      </MediaQuery> */}
-    </footer>
-  );
+  return <footer className={style.footer}></footer>;
 }
